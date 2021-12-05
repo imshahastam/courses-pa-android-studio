f4c589150ad44d33f8830a918c2b12d29a30cbee
imshahastam <shahzada.stamova@gmail.com>
2021-12-06 00:42:15 +0600

    hw10 done

--- app/src/main/java/com/example/progaiymhomeworks/MainActivity.kt
+++ app/src/main/java/com/example/progaiymhomeworks/MainActivity.kt
@@ -9,50 +9,92 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import com.example.progaiymhomeworks.R.color.red
+import com.example.progaiymhomeworks.databinding.ActivityMainBinding
+import net.objecthunter.exp4j.ExpressionBuilder
+import java.lang.Exception
 
+//Shahzada Stamova
 class MainActivity : AppCompatActivity() {
 
-    private var count = 0
-    lateinit var textView: AppCompatTextView
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        textView = findViewById<AppCompatTextView>(R.id.textView)
-
-        if (savedInstanceState != null) {
-            val score = savedInstanceState.getInt("score")
-            count = score
-            textView.text = count.toString()
+        //numbers
+        binding.apply {
+            zeroBtn.setOnClickListener { inputView("0") }
+            oneBtn.setOnClickListener { inputView("1") }
+            twoBtn.setOnClickListener { inputView("2") }
+            threeBtn.setOnClickListener { inputView("3") }
+            fourBtn.setOnClickListener { inputView("4") }
+            fiveBtn.setOnClickListener { inputView("5") }
+            sixBtn.setOnClickListener { inputView("6") }
+            sevenBtn.setOnClickListener { inputView("7") }
+            eightBtn.setOnClickListener { inputView("8") }
+            nineBtn.setOnClickListener { inputView("9") }
         }
 
-        val countButton = findViewById<AppCompatButton>(R.id.countButton)
-        val resetButton = findViewById<AppCompatButton>(R.id.resetButton)
-
-        countButton.setOnClickListener{
-            ++count
-            textView.text = count.toString()
+        //operations
+        binding.apply {
+            plus.setOnClickListener { inputView("+") }
+            minus.setOnClickListener { inputView("-") }
+            divide.setOnClickListener { inputView("/") }
+            multiply.setOnClickListener { inputView("*") }
         }
 
-        resetButton.setOnClickListener{
-            count = 0
-            textView.text = count.toString()
+        //other
+        binding.apply {
+            cleanAllBtn.setOnClickListener {
+                inputTxt.text = ""
+                answerTxt.text = ""
+            }
+            bracketOpenBtn.setOnClickListener { inputView("(") }
+            bracketCloseBtn.setOnClickListener { inputView(")") }
+            dotBtn.setOnClickListener { inputView(".") }
+
+            backBtn.setOnClickListener {
+                val inptTxt = inputTxt.text.toString()
+                inputTxt.text = inptTxt.substring(0, inptTxt.length - 1)
+            }
         }
+
+        binding.apply {
+            equalBtn.setOnClickListener{
+                try {
+                    val expression = ExpressionBuilder(inputTxt.text.toString()).build()
+                    val result = expression.evaluate()
+                    val longResult = result.toLong()
+
+                    if (result == longResult.toDouble()) {
+                        answerTxt.text = longResult.toString()
+                    } else {
+                        answerTxt.text = result.toString()
+                    }
+                }catch (e: ArithmeticException) {
+                    answerTxt.text = "На 0 делить нельзя!"
+                } catch (e: Exception) {
+                    answerTxt.text = "Ошибка!"
+            }
+        }
+
 
     }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt("score", count)
+}
+    private fun inputView(text: String) {
+        binding.inputTxt.append(text)
     }
 }
