99014b84db4b0bae438d539a9482d361a597dcd2
imshahastam <shahzada.stamova@gmail.com>
2021-12-20 23:32:16 +0600

    add hw18

---
+++ app/src/main/java/com/example/progaiymhomeworks/FragmentAbout.kt
@@ -0,0 +1,27 @@
+package com.example.progaiymhomeworks
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+
+class FragmentAbout : Fragment(R.layout.fragment_about){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val toolbar = view.findViewById<Toolbar>(R.id.toolbarAbout)
+        toolbar.inflateMenu(R.menu.menu)
+
+        toolbar.setOnMenuItemClickListener {
+            when(it.itemId) {
+                R.id.back -> {
+                    requireActivity().supportFragmentManager.beginTransaction()
+                        .replace(R.id.frg_container, FragmentMain())
+                        .commit()
+                }
+            }
+            true
+        }
+    }
+}
