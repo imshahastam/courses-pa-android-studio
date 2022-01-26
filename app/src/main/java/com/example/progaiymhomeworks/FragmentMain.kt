49f78d2928e3370ebc6f76e9a94119e073f8e9a2
imshahastam <shahzada.stamova@gmail.com>
2022-01-26 15:51:12 +0600

    upd hw40

--- app/src/main/java/com/example/progaiymhomeworks/FragmentMain.kt
+++ app/src/main/java/com/example/progaiymhomeworks/FragmentMain.kt
@@ -7,7 +7,7 @@ import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.fragment.app.Fragment
 
-//экран, где данные должны отображаться
+//main экран, где данные должны отображаться
 class FragmentMain : Fragment(R.layout.fragment_main) {
 
     private val dbInstance get() = Injector.database
