d78c149b387e698faea6e7fae83c5dfe550941f4
imshahastam <shahzada.stamova@gmail.com>
2022-02-04 15:16:58 +0600

    add hw43

--- app/src/main/java/com/example/progaiymhomeworks/FragmentItemInfo.kt
+++ app/src/main/java/com/example/progaiymhomeworks/FragmentItemInfo.kt
@@ -1,17 +1,24 @@
 package com.example.progaiymhomeworks
 
 import android.content.Context
+import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
+import com.example.progaiymhomeworks.database.Employee
 
 class FragmentItemInfo : Fragment (R.layout.fragment_item_info) {
 
     private val dbInstance get() = Injector.database
     private lateinit var listener: OnButtonsClick
+    private lateinit var e: Employee
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -26,7 +33,7 @@ class FragmentItemInfo : Fragment (R.layout.fragment_item_info) {
         val salaryInfo = view.findViewById<AppCompatTextView>(R.id.salaryInfo)
 
         val id = arguments?.getLong("id")
-        val e = dbInstance.employeeDao().getById(id)
+        e = dbInstance.employeeDao().getById(id)
 
         nameInfo.text = e.name
         salaryInfo.text = e.salary.toString()
@@ -37,5 +44,37 @@ class FragmentItemInfo : Fragment (R.layout.fragment_item_info) {
             listener.openFragmentEdit(id)
         }
 
+        val deleteBtn = view.findViewById<AppCompatButton>(R.id.deleteBtn)
+        deleteBtn.setOnClickListener {
+            //dbInstance.employeeDao().delete(e)
+            showAlertDialog()
+        }
+
+    }
+
+    private fun showToast(messageRes: String) {
+        Toast.makeText(requireContext(), messageRes, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showAlertDialog() {
+        val listener = DialogInterface.OnClickListener { _, which ->
+            when (which) {
+                DialogInterface.BUTTON_POSITIVE -> {
+                    dbInstance.employeeDao().delete(e)
+                }
+                DialogInterface.BUTTON_NEGATIVE -> showToast("Don't delete")
+            }
+        }
+        val dialog = AlertDialog.Builder(requireContext())
+            .setCancelable(true)
+            .setTitle(R.string.alert_dialog_title)
+            .setPositiveButton("Yes", listener)
+            .setNegativeButton("No", listener)
+            .setOnCancelListener {
+                showToast("Cancel")
+            }
+            .create()
+
+        dialog.show()
     }
 }
